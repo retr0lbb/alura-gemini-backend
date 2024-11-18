1f1a16f46f38c2325e988bcd5e5281fff95ccb24
retr0lbb <retr0lbb@gmail.com>
2024-11-18 18:01:01 -0300

    feat:add code linter biomejs and configurations on repo

--- server.ts
+++ server.ts
@@ -3,13 +3,11 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-
-app.get("/", (request, reply) => {
-    reply.send("server response")
-    return;
-})
-
-app.listen(port, () => {
-    console.log(`server listening on port ${port}`);
+app.get("/api", (_request, reply) => {
+	reply.status(200).send("server response");
+	return;
 });
 
+app.listen(port, () => {
+	console.log(`server listening on port ${port}`);
+});
